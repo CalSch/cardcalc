610a355509b29ee16d9c947423be006f0c296c75
CalSch <calschwick@calschwick.net>
2024-12-27 13:26:58 -0600

    added key bindings

---
+++ keys.h
@@ -0,0 +1,7 @@
+#define CALC_KEY_ADD      '+'
+#define CALC_KEY_SUBTRACT '-'
+#define CALC_KEY_MULTIPLY '*'
+#define CALC_KEY_DIVIDE   '/'
+#define CALC_KEY_POWER    '^'
+
+#define CALC_KEY_DECIMAL_TOGGLE '.' // Toggle decimal mode
